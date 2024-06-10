643414717525262d5a39711f7db7291612854d00
mireira07 <mireira2000@hotmail.com>
2024-06-10 15:28:22 -0300

    projeto dos heróis

---
+++ projeto.js
@@ -0,0 +1,23 @@
+//nome do heroi.
+let heroi = "Miguel"
+//classes dos herois
+let classes = ["ferro","bronze","prata","ouro","platina","ascendente","imortal","radiante"]
+//níveis de xp para classificar a classe do heroi.
+let xp = 7777
+if(xp<1000){
+    console.log("O Herói de nome "+heroi+" está no nível de "+classes[0])
+}else if(xp >= 1001 && xp <= 2000){
+      console.log("O Herói de nome "+heroi+" está no nível de "+classes[1])
+}else if(xp >= 2001 && xp <= 5000){
+    console.log("O Herói de nome "+heroi+" está no nível de "+classes[2])
+}else if(xp >= 5001 && xp <= 7000){
+    console.log("O Herói de nome "+heroi+" está no nível de "+classes[3])}
+    else if(xp >= 7001 && xp <= 8000){
+        console.log("O Herói de nome "+heroi+" está no nível de "+classes[4])}
+        else if(xp >= 8001 && xp <= 9000){
+            console.log("O Herói de nome "+heroi+" está no nível de "+classes[5])}
+            else if(xp >= 9001 && xp <= 10000){
+                console.log("O Herói de nome "+heroi+" está no nível de "+classes[6])}
+                else if(xp >1001){
+                    console.log("O Herói de nome "+heroi+" está no nível de "+classes[7])}
+                   
